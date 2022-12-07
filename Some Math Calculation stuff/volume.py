d3015f0d16f2c451d16f291120d8e33957232af8
Carrotoplia <carrotopliagithub@gmail.com>
2022-12-07 15:27:47 +0000

    ghhgfk

--- Some Math Calculation stuff/volume.py
+++
@@ -1,9 +0,0 @@
-OneOverThree = float(1/3)
-PI = float(3.14)
-Volume = float(input("Volume "))
-Height = float(input("Height "))
-
-Radius = OneOverThree * Volume / PI / Height
-
-RadiusSquared = Radius**float(2)
-print(OneOverThree * PI * RadiusSquared * Height)
