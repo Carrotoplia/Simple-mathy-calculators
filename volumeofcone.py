9f9d2e15ade912155d90a963307518171368339a
Carrotoplia <carrotopliagithub@gmail.com>
2022-12-07 15:27:18 +0000

    oops shouldnt have uploaded

--- volumeofcone.py
+++
@@ -1,7 +0,0 @@
-OneOverThree = float(1/3)
-PI = float(3.14)
-RadiusSquared = float(input("Radius "))**float(2)
-Height = float(input("Height "))
-
-print(RadiusSquared)
-print(OneOverThree * PI * RadiusSquared * Height)
